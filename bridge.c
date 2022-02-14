9debf1b57a41a04c90a259576f0fe00351f49877
Jaromír Wysoglad <jwysogla@redhat.com>
2022-02-14 10:50:44 +0100

    Fix race condition in link credit stat computation

--- bridge.c
+++ bridge.c
@@ -306,6 +306,7 @@ int main(int argc, char **argv) {
     long last_overrun = 0;
     long last_out = 0;
     long last_sock_overrun = 0;
+	long last_link_credit = 0;
 
     long sleep_count = 1;
 
@@ -319,17 +320,17 @@ int main(int argc, char **argv) {
                    app.sock_sent, app.sock_sent - last_out,
                    app.sock_would_block,
                    app.sock_would_block - last_sock_overrun,
-                   app.link_credit /
+                   (app.link_credit - last_link_credit) /
                        (float)(app.amqp_received - last_amqp_received));
 
             sleep_count = 1;
         }
         sleep_count++;
-        app.link_credit = 0;
         last_amqp_received = app.amqp_received;
         last_overrun = app.rbin->overruns;
         last_out = app.sock_sent;
         last_sock_overrun = app.sock_would_block;
+		last_link_credit = app.link_credit;
 
         if (app.socket_snd_th_running == 0) {
             pthread_join(app.socket_snd_th, NULL);
